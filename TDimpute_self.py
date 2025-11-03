fe062d4b53feff6ab359c5dc8ed5de2c237858a4
Ziad <ziadd.essam03@gmail.com>
2025-11-03 19:52:58 +0200

    add some comments for clarfication and read just the first 1000 rows

--- TDimpute_self.py
+++ TDimpute_self.py
@@ -4,11 +4,11 @@ import numpy as np
 import time
 import os
 
-def get_next_batch(dataset1, batch_size_1, step, ind):
-    start = step * batch_size_1
-    end = ((step + 1) * batch_size_1)
-    sel_ind = ind[start:end]
-    newdataset1 = dataset1[sel_ind, :]
+def get_next_batch(dataset1, batch_size_1, step, ind): # dataset, 10, 0, [0,1,2,3,4,5,6,7,8,9]
+    start = step * batch_size_1 # 0, 1
+    end = ((step + 1) * batch_size_1) # 10, 20
+    sel_ind = ind[start:end] # [0,1,2,3,4,5,6,7,8,9], [10,11,12,13,14,15,16,17,18,19]
+    newdataset1 = dataset1[sel_ind, :] # dataset[[0,1,2,3,4,5,6,7,8,9], :], dataset[[10,11,12,13,14,15,16,17,18,19], :]
     return newdataset1
 
 
@@ -28,9 +28,10 @@ def train(drop_prob, dataset_train, dataset_test, sav=True, checkpoint_file='def
     original = tf.placeholder(tf.float32, batch_shape_output, name='original')
     loss = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(reconstructed_image, original))))
     l2_loss = tf.losses.get_regularization_loss()
-    optimizer = tf.train.AdamOptimizer(lr).minimize(loss + l2_loss)
+    optimizer = tf.train.AdamOptimizer(lr).minimize(loss + l2_loss) # runs backpropagation and updates the weights
 
-    init = tf.global_variables_initializer()
+    # set up the training variables and environment
+    init = tf.global_variables_initializer() # initialize all variables in the graph (weights and biases)
     saver = tf.train.Saver()
     start = time.time()
     loss_val_list_train = 0
@@ -38,8 +39,8 @@ def train(drop_prob, dataset_train, dataset_test, sav=True, checkpoint_file='def
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    with tf.Session(config=config) as session:
-        session.run(init)
+    with tf.Session(config=config) as session: # open the session to run the computation graph
+        session.run(init) # give random initial values to all variables
 
         dataset_size_train = dataset_train.shape[0]
         dataset_size_test = dataset_test.shape[0]
@@ -48,22 +49,25 @@ def train(drop_prob, dataset_train, dataset_test, sav=True, checkpoint_file='def
         num_iters = (num_epochs * dataset_size_train) // batch_size
         print("Num iters:", num_iters)
         ind_train = []
+
+        # Generate for every epoch a random training samples,
         for i in range(num_epochs):
             ind_train = np.append(ind_train, np.random.permutation(np.arange(dataset_size_train)))
         ind_train = np.asarray(ind_train).astype("int32")
 
         total_cost_train = 0.
         num_batchs = dataset_size_train // batch_size  # "//" => int()
-        for step in range(num_iters):
+        for step in range(num_iters): # num of iteration cover all batches and epochs
+            ############### train section  ###########################
             temp = get_next_batch(dataset_train, batch_size, step, ind_train)
             train_batch = np.asarray(temp).astype("float32")
 
-            train_loss_val, _ = session.run([loss, optimizer],
+            train_loss_val, _ = session.run([loss, optimizer],  # compute loss and do the optimization (backpropagation) for one batch
                                             feed_dict={input_image: train_batch[:, 19027:],
                                                        original: train_batch[:, :19027],
                                                        is_training: True})
-            loss_val_list_train = np.append(loss_val_list_train, train_loss_val)
-            total_cost_train += train_loss_val
+            loss_val_list_train = np.append(loss_val_list_train, train_loss_val) # append the training loss value for this batch
+            total_cost_train += train_loss_val # accumulate the training loss value
 
             print_epochs = 10
             if step % (num_batchs * print_epochs) == 0:  # by epoch, num_batchs * batch_size = dataset_train_size
@@ -75,10 +79,10 @@ def train(drop_prob, dataset_train, dataset_test, sav=True, checkpoint_file='def
                 loss_val_list_test = np.append(loss_val_list_test, test_loss_val)
 
                 reconstruct = session.run(reconstructed_image,
-                                          feed_dict={input_image: dataset_test[:, 19027:], is_training: False})
-                nz = dataset_test[:, :19027].shape[0] * dataset_test[:, :19027].shape[1]
+                                          feed_dict={input_image: dataset_test[:, 19027:], is_training: False}) # get the predicted gene expression values for test set
+                nz = dataset_test[:, :19027].shape[0] * dataset_test[:, :19027].shape[1] # total number of GE values in test set (no of samples * 19K )
                 diff_mat = ((reconstruct  - dataset_test[:, :19027] ) * 16.5) ** 2
-                loss_test = np.sqrt(np.sum(diff_mat) / nz)
+                loss_test = np.sqrt(np.sum(diff_mat) / nz) # compute the RMSE 
 
                 print('RMSE loss by train_data_size: ', step, "/", num_iters,
                       total_cost_train / (num_batchs * print_epochs),
@@ -128,17 +132,20 @@ imputed_dataset_path = sys.argv[4]
 datadir = os.path.dirname(os.path.abspath(full_dataset_path))
 
 
-sample_size = 5
+sample_size = 1 # was 5 
 loss_list = np.zeros([16, 5, sample_size]) # 16 cancers, 5 missing rates, sample_size repeats
 loss_summary = np.zeros([16, 5]) # 16 cancers, 5 missing rates
 cancer_c = 0
 for cancertype in cancer_names:
     perc = 0
-    for missing_perc in [0.1,0.3,0.5,0.7,0.9]:
+    for missing_perc in [0.5]:   #  [0.1,0.3,0.5,0.7,0.9] # the percentage control the split between train and test data
         for sample_count in range(1,sample_size+1): # What is sample_count used for?
             # shuffle_cancer = pd.read_csv(datadir+'/full_datasets/' + cancertype + str(int(missing_perc * 100)) + '_' + str(sample_count) + '.csv',
             #     delimiter=',', index_col=0, header=0)
-            shuffle_cancer = pd.read_csv(full_dataset_path, delimiter=',', index_col=0, header=0)
+            shuffle_cancer = pd.read_csv(full_dataset_path, delimiter=',', index_col=0, header=0, nrows=1000)
+
+            # loop around 4 times getting chunk of 2k rows and do normalization and concateate
+            
             print('name:', cancertype, ' missing rate:', missing_perc, ' data size:',shuffle_cancer.shape)
             ########################Create set for training and testing
             ## 16.5 is for gene expression normalization // linearly scaled to [0, 1], the same as the DNA methylation data (beta values)
